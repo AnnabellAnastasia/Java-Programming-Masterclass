3111be948ccf514a890854c358cd719c6888caff
Annabella Wilkins <46332421+AnnabellAnastasia@users.noreply.github.com>
2019-12-08 23:00:17 -0500

    Add files via upload

---
+++ Section 6: OOP - Classes, Constructors and Inheritance/Classes/src/com/timbuchalka/Main.java
@@ -0,0 +1,14 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("Carrera");
+
+
+
+    }
+}
